a55330f5dd2855100805f9763288a45ee40baed5
GharudxD <76030990+GharudxD@users.noreply.github.com>
2025-02-26 00:54:54 +0530

    for display images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
     output: "export",
-     images: {
+    images: {
         unoptimized: true
-    }
+    },
+    basePath: isProd ? "/portfolio-website" : "",
+    assetPrefix: isProd ? "/portfolio-website/" : "",
 };
 
 export default nextConfig;
