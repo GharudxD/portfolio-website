7aee689f07b6c1de895f315f759824e86ff3c734
vinitshah101 <76030990+vinitshah101@users.noreply.github.com>
2025-03-02 19:43:05 +0530

    [FIX] new doamin setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
     output: "export",
     images: {
-        unoptimized: true
+      unoptimized: true,
     },
-    basePath: isProd ? "/portfolio-website" : "",
-    assetPrefix: isProd ? "/portfolio-website/" : "",
-};
-
-export default nextConfig;
+    basePath: "", // or remove this property
+    assetPrefix: "", // or remove this property
+  };  
